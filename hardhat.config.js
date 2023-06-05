81820fa515e097a7d9e4dd116d6446344b64d35a
marklin2289 <marklin2289@gmail.com>
2023-06-05 12:59:46 -0700

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: {
+    compilers: [{ version: "0.7.6" }],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 5000,
+        details: { yul: false },
+      },
+    },
+  },
+};
